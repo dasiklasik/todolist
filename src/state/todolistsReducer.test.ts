02b15c10cba0fac4628f92a9b910c8763f516908
dasiklasik <daryasamsonowicz@gmail.com>
2022-07-23 21:31:13 +0300

    write test for new AC

--- src/state/todolistsReducer.test.ts
+++ src/state/todolistsReducer.test.ts
@@ -1,18 +1,22 @@
-import { v1 } from "uuid"
+import {v1} from "uuid"
 import {
     addTodolist, changeFilter,
     changeFilterType, changeTodolistTitle,
     changeTodolistTitleType,
     FilterValuesType,
-    removeTodolist, removeTodolistType, TodolistDomainType,
+    removeTodolist, removeTodolistType, setTodolist, TodolistDomainType,
     todolistsReducer
 } from "./todolistsReducer";
+import {todolistAPI, TodolistType} from "../api/todolistAPI";
 
-test('correct todolist should be removed', () => {
-    let todolistId1 = v1()
-    let todolistId2 = v1()
 
-    const startState: Array<TodolistDomainType> =[
+let todolistId1 = v1()
+let todolistId2 = v1()
+
+let startState: Array<TodolistDomainType>;
+
+beforeEach(() => {
+    return startState = [
         {
             id: todolistId1,
             title: 'What to learn',
@@ -28,6 +32,10 @@ test('correct todolist should be removed', () => {
             order: 0,
         },
     ]
+})
+
+test('correct todolist should be removed', () => {
+
 
     const action: removeTodolistType = removeTodolist(todolistId1)
 
@@ -39,27 +47,10 @@ test('correct todolist should be removed', () => {
 })
 
 test('correct todolist should be added', () => {
-    let todolistId1 = v1();
-    let todolistId2 = v1();
+
 
     let newTodolistTitle = "New Todolist";
 
-    const startState: Array<TodolistDomainType> = [
-        {
-            id: todolistId1,
-            title: 'What to learn',
-            filter: 'all',
-            addedDate: '',
-            order: 0,
-        },
-        {
-            id: todolistId2,
-            title: 'What to buy',
-            filter: 'all',
-            addedDate: '',
-            order: 0,
-        },
-    ]
     let newID = v1()
     const action = addTodolist(newID, newTodolistTitle)
 
@@ -71,29 +62,11 @@ test('correct todolist should be added', () => {
 
 
 test('correct todolist should change its name', () => {
-    let todolistId1 = v1();
-    let todolistId2 = v1();
 
     let newTodolistTitle = "New Todolist";
 
-    const startState: Array<TodolistDomainType> = [
-        {
-            id: todolistId1,
-            title: 'What to learn',
-            filter: 'all',
-            addedDate: '',
-            order: 0,
-        },
-        {
-            id: todolistId2,
-            title: 'What to buy',
-            filter: 'all',
-            addedDate: '',
-            order: 0,
-        },
-    ]
 
-    const action: changeTodolistTitleType  = changeTodolistTitle(todolistId2, newTodolistTitle)
+    const action: changeTodolistTitleType = changeTodolistTitle(todolistId2, newTodolistTitle)
 
     const endState = todolistsReducer(startState, action);
 
@@ -102,27 +75,8 @@ test('correct todolist should change its name', () => {
 });
 
 test('correct filter of todolist should be changed', () => {
-    let todolistId1 = v1();
-    let todolistId2 = v1();
 
     let newFilter: FilterValuesType = "completed";
-
-    const startState: Array<TodolistDomainType> = [
-        {
-            id: todolistId1,
-            title: 'What to learn',
-            filter: 'all',
-            addedDate: '',
-            order: 0,
-        },
-        {
-            id: todolistId2,
-            title: 'What to buy',
-            filter: 'all',
-            addedDate: '',
-            order: 0,
-        },
-    ]
 
 
     const action: changeFilterType = changeFilter(todolistId2, newFilter)
@@ -132,5 +86,21 @@ test('correct filter of todolist should be changed', () => {
     expect(endState[0].filter).toBe("all");
     expect(endState[1].filter).toBe(newFilter);
 });
+
+test('todolists should be set', () => {
+
+    let endState:Array<TodolistType>
+
+    todolistAPI.getTodolist()
+        .then(response => {
+            endState = todolistsReducer(startState, setTodolist(response))
+            expect(endState[0].id).toBe('72f89144-c8df-44ca-b9b0-e00b83d06b33')
+        })
+
+
+
+
+
+})
 
 
